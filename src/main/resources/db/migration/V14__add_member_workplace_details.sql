86a654b4a24088f8f5c8e63d3a3eab489f64b712
hwang-hyesung <hwanghs7008@g.skku.edu>
2026-07-22 16:59:25 +0900

    수정: 회원 직장정보 마이그레이션 H2 호환

--- src/main/resources/db/migration/V14__add_member_workplace_details.sql
+++ src/main/resources/db/migration/V14__add_member_workplace_details.sql
@@ -2,8 +2,8 @@ ALTER TABLE users ADD COLUMN work_zipcode VARCHAR(50);
 ALTER TABLE users ADD COLUMN work_address1 VARCHAR(255);
 ALTER TABLE users ADD COLUMN work_address2 VARCHAR(255);
 
-UPDATE users u
-JOIN companies c ON c.id = u.company_id
-SET u.work_zipcode = c.work_zipcode,
-    u.work_address1 = c.work_address1,
-    u.work_address2 = c.work_address2;
+UPDATE users
+SET work_zipcode = (SELECT c.work_zipcode FROM companies c WHERE c.id = users.company_id),
+    work_address1 = (SELECT c.work_address1 FROM companies c WHERE c.id = users.company_id),
+    work_address2 = (SELECT c.work_address2 FROM companies c WHERE c.id = users.company_id)
+WHERE company_id IS NOT NULL;
